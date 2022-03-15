7761f0253cbd6b6ce7e53b0607bcd0e292c2a6f9
unravelet <76056657+unravelet@users.noreply.github.com>
2022-03-15 14:41:57 +0100

    fixed namespace

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace swen2_TourPlanner
+namespace TourPlanner
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
