fea263f830947690a9bfb4d7f7d14b9338f0b07d
Stefan Düx <if20b245@technikum-wien.at>
2022-03-22 14:51:25 +0100

    Delegate command basics umgesetzt

---
+++ DelegateCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace swen2_TourPlanner
+{
+    public class DelegateCommand : ICommand
+    {
+        readonly Action<object> execute;
+        readonly Predicate<object> canExecute;
+
+        public DelegateCommand(Predicate<object> canExecute, Action<object> execute) =>
+            (this.canExecute, this.execute) = (canExecute, execute);
+
+        public DelegateCommand(Action<object> execute) : this(null, execute) { }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter) => canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object? parameter) => execute?.Invoke(parameter);
+    }
+}
