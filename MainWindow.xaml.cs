e4e54f074a2e53a7df59f9971935379042d706ca
unravelet <76056657+unravelet@users.noreply.github.com>
2022-03-23 11:41:23 +0100

    added bl dal models project

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,21 +8,9 @@ namespace swen2_TourPlanner {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            this.DataContext = new MainViewModel();
+            DataContext = new MainViewModel();
         }
 
-        private void Loupe_Click(object sender, RoutedEventArgs e) {
-            MessageBox.Show("I was clicked");
-        }
-
-        private void OpenFile_Click(object sender, RoutedEventArgs e) {
-            string s = null;
-            try {
-                s.Trim();
-            }
-            catch (Exception ex) {
-                MessageBox.Show("Handled exception occured:" + ex.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
-            }
-        }
+        
     }
 }
