76748cb4bb8b1b4b0101be2c5662192fdef44958
Stefan Düx <if20b245@technikum-wien.at>
2022-03-30 16:54:49 +0200

    committen

--- TourPlanner/MainWindow.xaml.cs
+++ TourPlanner/MainWindow.xaml.cs
@@ -8,8 +8,9 @@ namespace TourPlanner {
     /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
-            InitializeComponent();
             DataContext = new MainViewModel(new Businesslogic());
+            InitializeComponent();
+            
         }
 
         
