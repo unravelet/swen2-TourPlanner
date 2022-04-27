0b8f2fa9a1673f6f5e44ce8b16af5fa5117d7efd
unravelet <76056657+unravelet@users.noreply.github.com>
2022-04-27 13:04:17 +0200

    failed again

--- TourPlanner/ViewModels/NewTourViewModel.cs
+++ TourPlanner/ViewModels/NewTourViewModel.cs
@@ -7,6 +7,34 @@ using System.Threading.Tasks;
 namespace TourPlanner.ViewModels {
     public class NewTourViewModel : BaseViewModel {
 
+        public NewTourViewModel() {
+
+            CreateTourCommand = new DelegateCommand(
+                o => true,
+                o => {
+                    Name = "hello";
+                }
+                );
+
+        }
+
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string StartAddress { get; set; }
+        public string StartAddressNumber { get; set; }
+        public string StartZip { get; set; }
+        public string StartCountry { get; set; }
+
         
+        public string EndAddress { get; set; }
+        public string EndAddressNumber { get; set; }
+        public string EndZip { get; set; }
+        public string EndCountry { get; set; }
+
+        public DelegateCommand CreateTourCommand { get; set; }
+
+
+
     }
 }
